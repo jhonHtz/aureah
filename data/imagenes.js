f48a86f5f0e7032b188eb431e75d3282bcccba3c
Jonatan I Huitz <jonatanhuitz43@gmail.com>
2026-02-13 23:46:02 -0600

    :memo:Primer despliegue

---
+++ data/imagenes.js
@@ -0,0 +1,41 @@
+const imagenesDisponibles = [
+
+    //  XERJOFF
+    "erba-pura.png",
+    
+    //  COACH
+    "coach-mujer.png",
+    "coachlove-mujer.png",
+    
+    //  CAROLINA H
+    "rose.png",
+    "rose2.png",
+    "gg.png",
+    "ggb.png",
+    
+    //  YSL
+    "libreedp.png",
+    "yslblack.png",
+    "yslgris.png",
+    
+    //  DOLCE & GABANNA
+    "lightblue.png",
+    "devotion.png",
+    
+    //  STHEPANE
+    "god-of-fire.png",
+
+    //  RABANNE
+    "olympea.png",
+    "olymsolar.png",
+    "ladyroyal.png",
+    "fameintense.png",
+    "fame.png",
+
+    //  VERSACE
+    "dylan-purple.png",
+    "bcrystal.png",
+    
+    //  VALENTINO
+    "mujercoral.png",
+];
