1106ac4f3ed57ff82f549c5d017a9c7d2ec8afae
Jonatan I Huitz <jonatanhuitz43@gmail.com>
2026-02-14 00:04:55 -0600

    :memo: json perfumes actualizados

--- data/perfumes.js
+++ data/perfumes.js
@@ -1,27 +1,147 @@
 const perfumes = [
-    {
-        id: 1,
-        nombre: "Erba Pura",
-        marca: "Xerjoff",
-        subtitulo: "edp",
-        genero: "unisex",
-        precioOriginal: 4500,
-        precioFinal: 3690,
-        imagen: "assets/img/perfumes/erba-pura.png",
-        
-        notas: {
-        salida: ["bergamota de Calabria", "naranja siliciana"],
-        corazon: ["frutas"],
-        fondo: ["almizcle","vainilla de Madagascar","ámbar"]
-        },
 
-        uso: {
-            primavera: { porcentaje: 84, activo: true },
-            verano: { porcentaje: 95, activo: true },
-            otono: { porcentaje: 55, activo: false },
-            invierno: { porcentaje: 30, activo: false },
-            dia: { porcentaje: 85, activo: true },
-            noche: { porcentaje: 53, activo: true }
-        }
-    }
+{
+id: 1771032501110,
+marca: "COACH",
+nombre: "COACH CLASICO 90ml",
+subtitulo: "EDP",
+genero: "mujer",
+precioOriginal: 2650,
+precioFinal: 1290,
+imagen: "assets/img/perfumes/coach-mujer.png",
+notas: {
+    salida: ["piña","naranja","pimienta rosa","bergamota","limón (lima ácida)"],
+    corazon: ["gardenia","rosa de té","jazmín sambac (sampaguita)","hedione"],
+    fondo: ["almizcle","pachulí","notas amaderadas","vainilla","sándalo"]
+},
+uso: {
+    primavera:{porcentaje:93,activo:true},
+    verano:{porcentaje:80,activo:true},
+    otono:{porcentaje:38,activo:false},
+    invierno:{porcentaje:18,activo:false},
+    dia:{porcentaje:93,activo:true},
+    noche:{porcentaje:38,activo:false}
+}
+},
+
+{
+id: 1771033943589,
+marca: "COACH",
+nombre: "COACH LOVE 90ml",
+subtitulo: "EDP",
+genero: "mujer",
+precioOriginal: 2650,
+precioFinal: 1380,
+imagen: "assets/img/perfumes/coachlove-mujer.png",
+notas: {
+    salida: ["fresa silvestre"],
+    corazon: ["rosa roja"],
+    fondo: ["cedro"]
+},
+uso: {
+    primavera:{porcentaje:98,activo:true},
+    verano:{porcentaje:90,activo:true},
+    otono:{porcentaje:25,activo:false},
+    invierno:{porcentaje:8,activo:false},
+    dia:{porcentaje:93,activo:true},
+    noche:{porcentaje:35,activo:false}
+}
+},
+
+{
+id: 1771035305386,
+marca: "VERSACE",
+nombre: "DYLAN PURPLE 100ml",
+subtitulo: "EDP",
+genero: "mujer",
+precioOriginal: 3650,
+precioFinal: 1560,
+imagen: "assets/img/perfumes/dylan-purple.png",
+notas: {
+    salida: ["pera","bergamota","naranja amarga"],
+    corazon: ["fresia","pomarose","mahonial"],
+    fondo: ["ambroxan","Iso E","cedro de virginia","sylkolide","árbol de belambra"]
+},
+uso: {
+    primavera:{porcentaje:95,activo:true},
+    verano:{porcentaje:88,activo:true},
+    otono:{porcentaje:30,activo:false},
+    invierno:{porcentaje:13,activo:false},
+    dia:{porcentaje:93,activo:true},
+    noche:{porcentaje:30,activo:false}
+}
+},
+
+{
+id: 1771038579282,
+marca: "VERSACE",
+nombre: "BRIGHT CRYSTAL 90ml",
+subtitulo: "EDP",
+genero: "mujer",
+precioOriginal: 3050,
+precioFinal: 1380,
+imagen: "assets/img/perfumes/bcrystal.png",
+notas: {
+    salida: ["yuzu","granada","hielo"],
+    corazon: ["peonía","flor de loto","magnolia"],
+    fondo: ["almizcle","cedro","ámbar"]
+},
+uso: {
+    primavera:{porcentaje:93,activo:true},
+    verano:{porcentaje:88,activo:true},
+    otono:{porcentaje:25,activo:false},
+    invierno:{porcentaje:15,activo:false},
+    dia:{porcentaje:95,activo:true},
+    noche:{porcentaje:23,activo:false}
+}
+},
+
+{
+id: 1771039513266,
+marca: "DOLCE&GABBANA",
+nombre: "LIGHT BLUE 100ml",
+subtitulo: "EDT",
+genero: "mujer",
+precioOriginal: 3150,
+precioFinal: 1630,
+imagen: "assets/img/perfumes/lightblue.png",
+notas: {
+    salida: ["limón siliciano","manzana","cedro","campanilla"],
+    corazon: ["jazmín","rosa blanca","bambú"],
+    fondo: ["cedro","almizcle","ámbar"]
+},
+uso: {
+    primavera:{porcentaje:86,activo:true},
+    verano:{porcentaje:94,activo:true},
+    otono:{porcentaje:29,activo:false},
+    invierno:{porcentaje:16,activo:false},
+    dia:{porcentaje:94,activo:true},
+    noche:{porcentaje:34,activo:false}
+}
+},
+
+{
+id: 1771040466431,
+marca: "DOLCE&GABBANA",
+nombre: "DEVOTION 100ml",
+subtitulo: "EDP",
+genero: "mujer",
+precioOriginal: 3550,
+precioFinal: 2280,
+imagen: "assets/img/perfumes/devotion.png",
+notas: {
+    salida: ["limón confitado"],
+    corazon: ["panna cotta","flor de azahar","ron"],
+    fondo: ["vainilla"]
+},
+uso: {
+    primavera:{porcentaje:69,activo:true},
+    verano:{porcentaje:34,activo:false},
+    otono:{porcentaje:89,activo:true},
+    invierno:{porcentaje:94,activo:true},
+    dia:{porcentaje:96,activo:true},
+    noche:{porcentaje:66,activo:true}
+}
+}
+
 ];
